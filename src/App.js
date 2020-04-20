6923e99b13d1918f4fd0cba44376416be321856c
kiranKamath <k96white@gmail.com>
2020-04-20 17:23:46 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Home } from "./Containers/HomePage/Home";
-import { AddBoard } from "./Containers/CreateBoard/AddBoard";
-import { Header } from "./components/Header/Header";
-import { Board } from "./Containers/board/Board";
+import React from 'react';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faList, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+import './App.css';
+
+import Boards from './../src/pages/Boards/Boards';
+import CreateBoard from './../src/pages/CreateBoard/CreateBoard';
+import Layout from './../src/pages/Layout/Layout';
+import Board from '../src/pages/Board/Board';
+
+library.add(faList, faTrashAlt);
 
 function App() {
+  let routes = (
+    <Switch>
+      <Route path="/createboard" component={CreateBoard}></Route>
+      <Route path="/board/:boardId" component={Board}></Route>
+      <Route path="/" component={Boards}></Route>
+      <Redirect to="/" />
+    </Switch>
+  );
+
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/createboard" component={AddBoard} />
-        <Route path="/board/:name" component={Board} />
-        <Route exact path="*" component={Home} />
-      </Switch>
-    </Router>
+    <div className="App">
+      <Layout>
+        {routes}
+      </Layout>
+    </div>
   );
 }
 
-export default App;
+export default withRouter(App);
