f9b7f3ec12692ed36bbb9c4a3103fc9f3d6e943d
kiranKamath <k96white@gmail.com>
2020-04-27 00:23:14 +0530

    changes done

--- src/common/modal/Modal.js
+++ src/common/modal/Modal.js
@@ -1,9 +1,20 @@
-/* eslint-disable react/prop-types */
-import React from "react";
-import Classes from "./Modal.module.css";
+import React, { Component } from 'react';
+import styles from './Modal.css';
 
-export const Modal = ({ children }) => (
-  <div className={Classes.modalContainer}>
-    <div className={Classes.modal}>{children}</div>
-  </div>
-);
+import Backdrop from './../Backdrop/Backdrop';
+
+class Modal extends Component {
+    render() {
+        return (
+            <div>
+                <Backdrop />
+                <span className={styles.Close} onClick={this.props.close}>X</span>
+                <div className={styles.Modal}>
+                    {this.props.content}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Modal;
